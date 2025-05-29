82fbfb42d01886539944361a8c93bd14afaf5cef
Eduardo Macetas <eduardo.macetas@gmail.com>
2025-05-28 22:15:48 -0500

    Initial Commit

---
+++ SwiftUI-MovieApp/SwiftUI_MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MovieAppApp.swift
+//  SwiftUI-MovieApp
+//
+//  Created by Eduardo Macetas on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-MovieAppTests/SwiftUI_MovieAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MovieAppTests.swift
+//  SwiftUI-MovieAppTests
+//
+//  Created by Eduardo Macetas on 5/28/25.
+//
+
+import Testing
+@testable import SwiftUI_MovieApp
+
+struct SwiftUI_MovieAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
